3670b873ff417d9ab7a24226d062c8aa1247e7bc
piyushgupta-29 <72931916+piyushgupta-29@users.noreply.github.com>
2021-12-02 00:03:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function myFunction(){
-    document.body.style.backgroundColor= "red";
+let a=["./images/image1.jpg","./images/image2.jpg","./images/image3.jpg","./images/image4.jpg","./images/image5.jpg"];
+let i=0;
+function previous(){
+    if(i==0)
+        i=4;
+    else
+        i--;
+    $("#image").src=a[i];
 }
+function next(){
+    i=(i+1)%5;
+    $("#image").src=a[i];
+}
+let width=document.querySelector(".container-fluid");
+let height=document.querySelector(".container-fluid");
+
+console.log(width);
+console.log(height);
